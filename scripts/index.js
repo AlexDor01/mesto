2e6535c1f461cf75d6e361b3a6a57af4ad12f36a
AlexDor01 <77833596+AlexDor01@users.noreply.github.com>
2021-04-02 14:15:04 +0300

    с Богом

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,19 @@
 const popup = document.querySelector('.popup');
 const openPopupBtn = document.getElementById('open_popup_btn');
+const popupContainer = document.querySelector('.popup__container')
 const closePopupBtn = document.querySelector('.popup__close-button');
-const popupOverlay = document.querySelector('.popup__overlay')
-
+const popupOverlay = document.querySelector('.popup__overlay');
+let popupNameInput = popupContainer.querySelector('.form__info_name');
+let popupDescrInput = popupContainer.querySelector('.form__info_descr');
+let profile = document.querySelector('.profile');
+let profileInfo = document.querySelector('.profile__info');
+let profileName = profileInfo.querySelector('.profile__title');
+let profileDescr = profileInfo.querySelector('.profile__subtitle');
 
 function openPopup() {
-    popup.classList.add('popup_opened')
+    popupNameInput.value = profileName.textContent;
+  popupDescrInput.value = profileDescr.textContent;
+  popup.classList.add('popup_opened');
 }
 function closePopup() {
     popup.classList.remove('popup_opened')
@@ -19,3 +27,12 @@ closePopup();
 popupOverlay.addEventListener('click', function() {
 closePopup();
 })
+function formSubmitHandler(evt) {
+    evt.preventDefault();
+    profileName.textContent = popupNameInput.value;
+    profileDescr.textContent = popupDescrInput.value;
+    closePopup();
+  }
+  closePopupBtn.addEventListener('click', closePopup);
+  popupContainer.addEventListener('submit', formSubmitHandler);
+
