44998638f8de2228b36fb41ee1fe5ba31c66e8e9
AlexDor01 <77833596+AlexDor01@users.noreply.github.com>
2021-04-12 00:46:05 +0300

    g

--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,91 @@
 const popup = document.querySelector('.popup');
+const popupEdit = document.querySelector('.popup__edit');
+const popupAdd = document.querySelector('.popup__add');
 const openPopupBtn = document.querySelector('.profile__change');
+const openPopupAdd = document.querySelector('.profile__button');
 const popupContainer = document.querySelector('.popup__container');
-const closePopupBtn = document.querySelector('.popup__close-button');
+const closePopupBtn = popupEdit.querySelector('.popup__close-button');
+const closePopupAdd = popupAdd.querySelector('.popup__close-button');
 const popupNameInput = popupContainer.querySelector('.form__input_type_name');
 const popupDescrInput = popupContainer.querySelector('.form__input_type_descr');
+const popupPlaceInput = popupContainer.querySelector('.form__input_type_place');
+const popupLinkInput = popupContainer.querySelector('.form__input_type_link');
 const profile = document.querySelector('.profile');
 const profileInfo = document.querySelector('.profile__info');
 const profileName = profileInfo.querySelector('.profile__title');
 const profileDescr = profileInfo.querySelector('.profile__subtitle');
 const formElement = document.querySelector('.form');
 
-function openPopup() {
+
+openPopupBtn.addEventListener('click', function () {
+  openPopup(popupEdit);
   popupNameInput.value = profileName.textContent;
-  popupDescrInput.value = profileDescr.textContent; 
+  popupDescrInput.value = profileDescr.textContent;
+});
+openPopupAdd.addEventListener('click', function () {
+  openPopup(popupAdd);
+});
+function openPopup(popup) {
   popup.classList.add('popup_opened');
 }
-function closePopup() {
+function closePopup(popup) {
   popup.classList.remove('popup_opened')
 }
-openPopupBtn.addEventListener('click', openPopup);
+
+closePopupAdd.addEventListener('click', function () {
+  closePopup(popupAdd);
+});
+closePopupBtn.addEventListener('click', function () {
+  closePopup(popupEdit);
+});
+
+
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = popupNameInput.value;
   profileDescr.textContent = popupDescrInput.value;
-  closePopup();
+  closePopup(popup);
 }
-closePopupBtn.addEventListener('click', closePopup);
+
 formElement.addEventListener('submit', handleFormSubmit);
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+function handlerAddFormSubmit(evt) {
+  const InputPlaceAdd = popupPlaceInput.value;
+  const InputLinkAdd = popupLinkInput.value;
+
+  const cardElement = cardElementTemplate.cloneNode(true);
+  const cardElementName = cardElement.querySelector('.elements__title');
+  cardElementName.textContent = InputPlaceAdd.name;
+  const cardElementImg = cardElement.querySelector('.elements__img');
+  cardElementImg.src = InputLinkAdd.link;
+  closePopup(popupAdd);
+  cardElementList.prepend(cardElement);
+}
+popupAdd.addEventListener('submit', handlerAddFormSubmit);
