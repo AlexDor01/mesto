d2d06deecc0810a8b98d99e7e8fb066835028bcc
AlexDor01 <77833596+AlexDor01@users.noreply.github.com>
2021-05-04 18:06:01 +0300

    e

--- scripts/ind.js
+++ scripts/ind.js
@@ -1,6 +1,7 @@
 const popup = document.querySelector('.popup');
 const popupEdit = document.querySelector('.popup__edit');
 const popupAdd = document.querySelector('.popup__add');
+const popupAddForm = document.querySelector('.popup-add-form');
 const openPopupBtn = document.querySelector('.profile__change');
 const openPopupAdd = document.querySelector('.profile__button');
 const popupContainer = document.querySelector('.popup__container');
@@ -16,11 +17,10 @@ const profileName = profileInfo.querySelector('.profile__title');
 const profileDescr = profileInfo.querySelector('.profile__subtitle');
 const formElement = document.querySelector('.form');
 
-//карточки
 const cardsContainer = document.querySelector('.elements');
 const cardTemplate = document.querySelector('.elements-item-template').content.querySelector('.elements__item');
 
-
+//Открытие и закрытие попапов
 openPopupBtn.addEventListener('click', function () {
   openPopup(popupEdit);
   popupNameInput.value = profileName.textContent;
@@ -43,14 +43,12 @@ closePopupBtn.addEventListener('click', function () {
   closePopup(popupEdit);
 });
 
-
 function handleFormSubmit(evt) {
   evt.preventDefault();
   profileName.textContent = popupNameInput.value;
   profileDescr.textContent = popupDescrInput.value;
   closePopup(popup);
 }
-
 formElement.addEventListener('submit', handleFormSubmit);
 
 const initialCards = [
@@ -80,6 +78,47 @@ const initialCards = [
   }
 ];
 
+function handlerFormSubmitEdit(evt) {
+  evt.preventDefault();
+  profileNmae.textContent = popupNameInput.value;
+  profileDescr.textContent = popupDescrInput.value;
+  closePopup(popup);
+}
+
+function handlerFormSubmitAdd(evt) {
+  evt.prevenrDefault();
+  addCardList({ name: popupInputPlaceAdd.value, link: popupInputLinkAdd.value }
+  );
+  popupAddForm.reset();
+  closePopup(popupAdd);
+}
+
+function handlerDelCard(evt) {
+  evt.target.closest('.elements__item').remove();
+}
+
+function handlerlikeBtn(evt) {
+  evt.target.classList.toggle('elements__like_active');
+}
+
+function handlerImgPrv(link, name) {
+  popupImgPhoto.src = link;
+  popupimgPhoto.alt = name;
+  popupImgTitle.textContent = name;
+  openPopup(popupImg);
+};
+
+const elementItemList = document.querySelector('.elements__list');
+
+initialCards.forEach(item => {
+  initialCard(item);
+});
+
+function initialCard(item) {
+  const elementItem = createElement(item);
+  cardsContainer.prepend(elementItem);
+}
+
 function handlerAddFormSubmit(evt) {
   const InputPlaceAdd = popupPlaceInput.value;
   const InputLinkAdd = popupLinkInput.value;
@@ -92,10 +131,6 @@ function handlerAddFormSubmit(evt) {
   closePopup(popupAdd);
   cardElementList.prepend(cardElement);
 }
-popupAdd.addEventListener('submit', handlerAddFormSubmit);
-
-const elementItemTemplate = document.querySelector('.elements-item-template').content.querySelector('.elements__item');
-
 function initialCard(item) {
   const elementItem = cardTemplate.cloneNode(true);
   const elementItemName = elementItem.querySelector('.elements__title');
@@ -103,23 +138,11 @@ function initialCard(item) {
   const elementItemImg = elementItem.querySelector('.elements__img');
   elementItemImg.src = item.link;
   elementItemImg.alt = item.name;
+  const elementDelBtn = elementItem.querySelector('.elements__button');
+  const elementLikeBtn = elementItem.querySelector('.elements__like');
 
-  const LikeBtn = elementItem.querySelector('.elements__like');
-  LikeBtn.addEventListener('click', () => {
-    LikeBtn.classList.toggle('elements__like_active');
-  });
-
-  const delBtn = elementItem.querySelector('.elements__button');
-  delBtn.addEventListener('click', () => {
-    elementItem.remove();
-  });
-
-  cardsContainer.prepend(elementItem);
+  elementDelBtn.addEventListener('click', handlerDelCard);
+  elementLikeBtn.addEventListener('click', handlerlikeBtn);
+  elementItemImg.addEventListener('click', () => handlerImgPrv(item.link, item.name));
+  return elementItem;
 }
-initialCards.forEach(item => {
-initialCard(item);
-});
- function addCardList(data) {
-   const Item = createCard(data);
-   elementItemList.prepend()
- }
